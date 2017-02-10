cc353d8e24d2a5aa5f24046997de6d58a4191d65
Adam Shannon <adamkshannon@gmail.com>
2017-02-10 15:37:16 -0600

    prefix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -125,3 +125,22 @@ func (i *IntegerLiteral) TokenLiteral() string {
 func (i *IntegerLiteral) String() string {
 	return i.Token.Literal
 }
+
+// prefix exp
+type PrefixExpression struct {
+	Token token.Token // The prefix token, e.g. !
+	Operator string
+	Right Expression
+}
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
